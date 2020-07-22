be9bca6c81670ccd98a31fd77507097138ad6f14
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-07-22 12:38:59 +0530

    Updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
